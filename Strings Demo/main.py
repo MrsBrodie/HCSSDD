9e85d48135fd8ea3199b150e55eb3b252b330f27
MrsBrodie <109583970+MrsBrodie@users.noreply.github.com>
2023-11-25 13:55:36 +0000

    Add files via upload

---
+++ Strings Demo/main.py
@@ -0,0 +1,29 @@
+#Higher Computing Science
+#Manipulation of strings
+#demo file
+
+newstring=""
+
+for x in range(6):
+  inputstring=input("Enter character ")
+  newstring+=inputstring
+print(newstring)
+
+
+'''print(myString1[7:13])
+print("")
+
+
+'''
+'''print(myString1)
+print(myString2)
+print(myString3)
+
+
+myString="The quick brown fox jumps over the laxy dog"
+print(myString[0:3])'''
+
+
+
+
+
